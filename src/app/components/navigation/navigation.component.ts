323bc2fd4a88f5e6c15bd58c59b80cc8e9bb7b78
Valeriy Danets <dantesvv@gmail.com>
2023-10-30 12:03:16 +0200

    Was implemented standalone SearchComponent

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SearchComponent],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  Observable,
+  Subject,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+})
+export class SearchComponent {
+  search = new FormControl('');
+  result$: Observable<string>;
+  private searchSubject = new Subject<string>();
+  subject$ = this.searchSubject.asObservable();
+
+  constructor() {
+    this.result$ = this.subject$.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((searchText) => {
+        return of(`Result for "${searchText}"`);
+      })
+    );
+  }
+
+  onSearch() {
+    this.searchSubject.next(this.search.value);
+  }
+}
